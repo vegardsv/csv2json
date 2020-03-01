ec7d5c58769302617a63056ce6eaae1c42a82463
Vegard Svendsen <vegardsvendsen@Vegards-MacBook-Pro-2.local>
2020-03-01 12:04:13 +0100

    major refactor

--- index.js
+++ index.js
@@ -1,56 +1,35 @@
 const fs = require("fs");
-const readline = require("readline");
-
-function printObject(keyValuePairs) {
-  return keyValuePairs
-    .reduce(
-      (acc, cur, idx) =>
-        acc.concat(
-          `"${cur[0]}":"${cur[1]}"${
-            idx !== keyValuePairs.length - 1 ? "," : ""
-          }`
-        ),
-      "{"
-    )
-    .concat("},");
-}
-
-function toPairs(xs1, xs2) {
-  return xs1.map((x, i) => [x, xs2[i]]);
-}
-
-async function process({ input, outfile, delimiter, pretty }) {
-  let output = "";
-  const fileStream = fs.createReadStream(input);
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-  let headers = undefined;
-  for await (const line of rl) {
-    if (!headers) {
-      headers = line.split(delimiter);
-    } else {
-      let values = line.split(delimiter);
-      output += printObject(toPairs(headers, values));
-    }
-  }
-  output = output.substring(0, output.length - 1);
-  fs.writeFile(
-    outfile,
-    pretty
-      ? JSON.stringify(JSON.parse("[" + output + "]"), null, 2)
-      : "[" + output + "]",
-    "utf8",
-    () => {
-      console.log("Job finished");
-    }
-  );
-}
 
 process({
-  input: "./MOCK_DATA.csv",
+  infile: "./MOCK_DATA.csv",
   outfile: "output.json",
   delimiter: ",",
   pretty: true
 });
+
+function process({ infile, outfile, delimiter, pretty }) {
+  fs.readFile(infile, "utf8", (err, input) => {
+    const lines = input.split("\n");
+    const headers = lines[0].split(delimiter);
+    const output = lines
+      .filter((_, i) => i !== 0)
+      .map((line, i) => {
+        values = line.split(delimiter);
+        return "{"
+          .concat(values.map((value, i) => `"${headers[i]}":"${value}"`))
+          .concat("}");
+      });
+    fs.writeFile(
+      outfile,
+      pretty ? format("[" + output + "]") : "[" + output + "]",
+      "utf8",
+      () => {
+        console.log("Job finished");
+      }
+    );
+  });
+}
+
+function format(input) {
+  return JSON.stringify(JSON.parse(input), null, 2);
+}
