b0755ee5e02a732add8713758a6756aea1313785
Vegard Svendsen <vegardsvendsen@Vegards-MacBook-Pro-2.local>
2020-04-03 10:34:44 +0200

    removes line-break from parsed text

--- csv2json.js
+++ csv2json.js
@@ -8,13 +8,16 @@ process({
 });
 
 function process({ infile, outfile, delimiter, pretty }) {
+  const toLine = line =>
+    line.split(delimiter).map(xs => xs.replace(/(\r\n|\n|\r)/gm, ""));
+
   fs.readFile(infile, "utf8", (err, input) => {
     const lines = input.split("\n");
-    const headers = lines[0].split(delimiter);
+    const headers = toLine(lines[0]);
     const output = lines
       .filter((_, i) => i !== 0)
       .map(line => {
-        values = line.split(delimiter);
+        values = toLine(line);
         return (
           "{" + values.map((value, i) => `"${headers[i]}":"${value}"`) + "}"
         );
